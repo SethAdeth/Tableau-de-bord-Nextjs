376a8623f6b3c4229f8ec64563f23c5f607f4716
Adeth <sethmessan@gmail.com>
2023-12-06 15:11:05 +0000

    final update v.0.1

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -172,6 +172,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
